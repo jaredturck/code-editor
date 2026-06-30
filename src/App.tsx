d2f9cef847b461d8596c959ef8f0ab9f1c3e3f92
Jared Turck <jaredturck8@gmail.com>
2026-06-30 11:14:52 +0100

    Added react templates

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-white p-8 text-black">
+      <h1 className="text-2xl font-semibold">
+        Hello World
+      </h1>
+    </main>
+  )
+}
+
+export default App
